eb170245dcb5030682ee7ee136bc6fbd22f75da7
DavidMMFon <115725195+DavidMMFon@users.noreply.github.com>
2023-07-13 17:30:01 +0100

    .

---
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.cpp
@@ -0,0 +1,8 @@
+#include "GameScreenLevel1.h"
+
+#include "Texture2D.h"
+
+GameScreenLevel1::GameScreenLevel1(SDL_Renderer* renderer) : GameScreen(renderer)
+{
+	SetUpLevel();
+}
---
+++ BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef GAMESCREENLEVEL1_H
+#define GAMESCREENLEVEL!_H
+
+#include "GameScreen.h"
+#include "Commons.h"
+
+class Texture2D;
+
+class GameScreenLevel1 : GameScreen
+{
+	private:
+		Texture2D* m_background_texture;
+
+		bool SetUpLevel();
+
+	public:
+		GameScreenLevel1(SDL_Renderer* renderer);
+		~GameScreenLevel1();
+
+		void Render() override;
+		void Update(float deltaTime, SDL_Event e) override;
+
+};
+
+#endif 
